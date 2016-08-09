9f1e1456dad7e331fa9e089966db6570cc71b4ab
Craig Vanderzwaag <craig@bluehulastudios.com>
2016-08-09 16:37:00 -0700

    Added Master Key

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://craigvz:blueHula@ds013931.mlab.com:13931/dispatch_test',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'dispatchtest1',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || '123456', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://dispatchtest1.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
