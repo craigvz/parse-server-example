20368efdf34b9f81a86a4703f9c6fba1335b5e31
Craig Vanderzwaag <craig@bluehulastudios.com>
2016-08-10 18:29:47 -0700

    fixed typo

--- cloud/main.js
+++ cloud/main.js
@@ -161,7 +161,7 @@ Parse.Cloud.afterSave ('IncidentDetails',function (request) {
                                        alert: [request.object.get("incidentType")] + ", " + [request.object.get("address1")] + ", " + [request.object.get("city")]+ ", " + [request.object.get("unitID")],
                                        lat: request.object.get("lat"),
                                        lon: request.object.get("lon"),
-                                       sound: "SBCPushAlert.wav"
+                                       sound: "SBCPushAlert.wav",
                                        mutable-content:"1"
                                        }
                                        }, {
