296b76a4182aa3798dff86d0c01b625beeb6cbe9
Craig Vanderzwaag <craig@bluehulastudios.com>
2016-08-10 18:12:49 -0700

    revert to original

--- cloud/main.js
+++ cloud/main.js
@@ -157,12 +157,8 @@ Parse.Cloud.afterSave ('IncidentDetails',function (request) {
                                        channels: request.object.get("channels"),
                                        data: {
                                       // alert: { "title": [request.object.get("incidentType")], "body": [request.object.get("address1")] + ", " + [request.object.get("city")], "subtitle": [request.object.get("unitID")]},
-                                       alert: {
-                                       "title" : "Introduction to Notifications",
-                                       "subtitle" : "Session 707",
-                                       "body" : "Woah! These new notifications look amazing! Don’t you agree?"
-                                       },
-                                   //    alert: [request.object.get("incidentType")] + ", " + [request.object.get("address1")] + ", " + [request.object.get("city")]+ ", " + [request.object.get("unitID")],
+                                    
+                                       alert: [request.object.get("incidentType")] + ", " + [request.object.get("address1")] + ", " + [request.object.get("city")]+ ", " + [request.object.get("unitID")],
                                        lat: request.object.get("lat"),
                                        lon: request.object.get("lon"),
                                        sound: "SBCPushAlert.wav"
