2606698cf2dc1ea4fb6a72a436a73f41b1632a2f
Craig Vanderzwaag <craig@bluehulastudios.com>
2016-08-13 16:21:57 -0700

    updated Main.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || '123', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://dispatchtest1.herokuapp.com/parse',
                           push: {
-                          adapter: parse
+                          adapter: parse-server-push-adapter
                           },
                           {
                           ios: [
